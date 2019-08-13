41de6d36b6a178e25323310ba853235c2c226ab4
grgpratik <gurung.pratik04@gmail.com>
2019-08-13 14:25:19 +0545

    created bloc for weather

---
+++ lib/bloc/weather_bloc.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutterweather/repositories/repositories.dart';
+import './bloc.dart';
+import 'package:flutterweather/model/models.dart';
+
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  final WeatherRepository weatherRepository;
+  WeatherBloc({@required this.weatherRepository})
+      : assert(weatherRepository != null);
+
+  @override
+  WeatherState get initialState => WeatherEmpty();
+
+  @override
+  Stream<WeatherState> mapEventToState(
+    WeatherEvent event,
+  ) async* {
+    if (event is FetchWeather) {
+      yield WeatherLoading();
+      try {
+        final Weather weather = await weatherRepository.getWeather(event.city);
+        yield WeatherLoaded(weather: weather);
+      } catch (_) {
+        yield WeatherError();
+      }
+    }
+  }
+}
